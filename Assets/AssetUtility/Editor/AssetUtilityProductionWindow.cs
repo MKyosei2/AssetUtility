09d76b5d335582b3ad8e56fe4a83f718f680c9f8
松野恭成(Kyosei Matsuno) <matsukyo958@gmail.com>
2026-06-05 22:02:37 +0900

    Add production AssetUtility editor window

---
+++ Assets/AssetUtility/Editor/AssetUtilityProductionWindow.cs
@@ -0,0 +1,70 @@
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace EasyTool
+{
+    public class AssetUtilityProductionWindow : EditorWindow
+    {
+        private Vector2 scrollPos;
+        private List<AssetInfo> scannedAssets;
+        private List<MeshOptimizationPlan> plans;
+        private AssetUtilityOptimizationReport report;
+
+        [MenuItem("Window/AssetUtility Production")] 
+        public static void ShowWindow() => GetWindow<AssetUtilityProductionWindow>("AssetUtility Production");
+
+        private void OnGUI()
+        {
+            GUILayout.Label("AssetUtility Production", EditorStyles.boldLabel);
+
+            if (GUILayout.Button("Scan Active Scene"))
+            {
+                scannedAssets = SceneScanService.ScanActiveScene();
+                report = new AssetUtilityOptimizationReport();
+                report.before = AssetCostEstimator.BuildSnapshot("before", scannedAssets);
+                report.StampNow("Scan");
+            }
+
+            if (scannedAssets != null && scannedAssets.Count > 0)
+            {
+                scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+                foreach (var asset in scannedAssets)
+                {
+                    EditorGUILayout.LabelField(asset.name + " [" + asset.objectType + "]");
+                }
+                EditorGUILayout.EndScrollView();
+
+                if (GUILayout.Button("Generate Mesh Plans"))
+                {
+                    plans = MeshOptimizationService.BuildPlansFromAssets(scannedAssets, true);
+                }
+
+                if (plans != null && plans.Count > 0)
+                {
+                    if (GUILayout.Button("Apply Plans (Dry Run)"))
+                    {
+                        foreach (var p in plans) AssetApplyService.ApplyMeshPlan(p, true);
+                        report.after = AssetCostEstimator.BuildSnapshot("after", scannedAssets);
+                        report.warnings = OptimizationRiskAnalyzer.Analyze(scannedAssets);
+                        JsonReportWriter.Write(report);
+                        MarkdownReportWriter.Write(report);
+                        CsvReportWriter.Write(report);
+                    }
+
+                    if (GUILayout.Button("Apply Plans (Actual)"))
+                    {
+                        foreach (var p in plans) AssetApplyService.ApplyMeshPlan(p, false);
+                        report.after = AssetCostEstimator.BuildSnapshot("after", scannedAssets);
+                        report.warnings = OptimizationRiskAnalyzer.Analyze(scannedAssets);
+                        JsonReportWriter.Write(report);
+                        MarkdownReportWriter.Write(report);
+                        CsvReportWriter.Write(report);
+                    }
+                }
+            }
+        }
+    }
+}
+#endif
